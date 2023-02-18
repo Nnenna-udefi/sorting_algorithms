10537c186c43b4e8c5acbf0242ee1532895cbbb1
Nnenna Udefi <miriamudefi@gmail.com>
2023-02-18 07:09:43 -0800

    header

--- sort.h
+++ sort.h
@@ -5,17 +5,6 @@
 #include <stdlib.h>
 
 /**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
- */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
-
-/**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
@@ -24,14 +13,12 @@ typedef enum bool
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-void swap_ints(int *a, int *b);
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
+
+/*print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
