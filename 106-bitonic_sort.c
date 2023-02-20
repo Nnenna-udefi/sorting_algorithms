5044232ba656ad48040521393b7f8f02e0ad60d2
Ifeanyit103 <ifeanyit103@gmail.com>
2023-02-20 21:00:16 +0000

    bitonic

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,12 +1,17 @@
 #include "sort.h"
 
 /**
-  * biotonic_sort - A function that sorts an array using biotonic algorithm
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
+ * bitonic_sort - Sort an array of integers in ascending
+ *                order using the bitonic sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap. Only works for
+ * size = 2^k where k >= 0 (ie. size equal to powers of 2).
+ */
 void bitonic_sort(int *array, size_t size)
 {
-	
+	if (array == NULL || size < 2)
+		return;
+
 }
