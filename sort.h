2cad67ba884b70d39881a93d8e5b655fc679b37c
Ifeanyit103 <ifeanyit103@gmail.com>
2023-02-20 17:26:20 +0000

    counting sort

--- sort.h
+++ sort.h
@@ -16,6 +16,8 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+void merge_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void bubble_sort(int *array, size_t size);
